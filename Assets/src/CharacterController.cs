866107f9126e1db6f778b3f93a165be6514bf49f
xteixeira <antoniolrt@gmail.com>
2016-12-10 01:30:40 -0200

    Add character movement

---
+++ Assets/src/CharacterController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+	public float speed = 0.2f;
+	Rigidbody2D body;
+	Vector3 input;
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		input = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
+
+		Move ();
+
+	}
+
+	void Move () {
+		body.velocity = input*speed;
+
+	}
+}
