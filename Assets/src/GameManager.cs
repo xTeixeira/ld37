072361648fdf905e8bee6ca82dac5690aaade243
leoleotte <leo_leotte@hotmail.com>
2016-12-10 05:41:31 -0200

    added GameManager, enemy tracking player

---
+++ Assets/src/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = player == null ? GameObject.Find ("Player") : player;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public static Vector3 GetPlayerPosition(){
+		return player.transform.position;
+	}
+}
