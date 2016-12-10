5fc737f22c245a7b03396323e8e01fac6b24159b
xteixeira <antoniolrt@gmail.com>
2016-12-10 14:36:10 -0200

    Final Chape sprites, fire animation, better colliders

---
+++ Assets/src/AimColliderListener.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimColliderListener : MonoBehaviour {
+
+	void OnTriggerStay2D (Collider2D col) {
+		Destroy (col.gameObject);
+	}
+}
